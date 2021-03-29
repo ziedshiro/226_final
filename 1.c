52d9c3d6a36a2932fef96ae114b325df4ebeba74
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-29 17:42:59 +0700

    1

--- 1.c
+++ 1.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
+int num(int n)
+{
+
+}
 int main()
 {
-    int n,m;
+    int n,m,ans=0;
     scanf("%d %d",&n,&m);
+    do
+    {
+        if(num(n)==0)
+            n++;
+        else
+            ans=1;
+    }while(ans!=1);
 }
