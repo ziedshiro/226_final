6681e934a9661456f62d0e449679113f22038e1f
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-29 17:14:41 +0700

    2

--- 2.c
+++ 2.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    int i,n,j;
+    int i,n,j,x,y;
     scanf("%d",&n);
     int N[n],max[n];
     for(i=0;i<n;i++)
@@ -12,11 +12,15 @@ int main()
     {
         for(j=0;j<n-1;j++)
     {
-        if(max[i]<max[i+1])
+        if(max[j]>max[j+1])
         {
-
+            x=max[j];
+            max[j]=max[j+1];
+            max[j+1]=x;
         }
     }
     }
+    for(i=0;i<n-1;i++)
+        printf("%d",&max[i]);
 
 }
