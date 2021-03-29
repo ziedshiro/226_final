91daf145de8b142a60298436b15141fcdac8d049
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-29 18:29:41 +0700

    3

--- 3.c
+++ 3.c
@@ -1,45 +1,51 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z,i,j,ans=0;
+    int x,y,z,i,j,ans=0,dd=0;
     scanf("%d %d %d",&x,&y,&z);
-    int a[x][x],b[y][y],c[z][z];
+    int a[x][x],b[y][y],c[z][z],r[x],cl[x],df=0,db=0;
     for(i=0;i<x;i++)
         for(j=0;j<x;j++)
-    scanf("%d",&a[i]);
+            scanf("%d",&a[i][j]);
     /*for(i=0;i<y;i++)
         for(j=0;j<y;j++)
     scanf("%d",&b[i]);
     for(i=0;i<z;i++)
         for(j=0;j<z;j++)
     scanf("%d",&c[i]);*/
-    int r[x],cl[x],d[x];
-    for(i=0;i<3;i++)
+    for(i=0;i<x;i++)
     {
         r[i]=0;
         cl[i]=0;
-        d[i]=0;
     }
     for(i=0;i<x;i++)
         for(j=0;j<x;j++)
-              r[i]+=a[i][j];
+            r[i]+=a[i][j];
     for(i=0;i<x;i++)
         for(j=0;j<x;j++)
             cl[i]+=a[j][i];
     for(i=0;i<x;i++)
         for(j=0;j<x;j++)
             if(i==j)
-            d[i]+=a[i][j];
+                df+=a[i][j];
+    for(i=0;i<x;i++)
+        for(j=x-1;j>=0;j--)
+            if(i==j)
+                db+=a[i][j];
+    if(df==db)
+        dd=1;
+    else
+        dd=0;
     for(i=0;i<x;i++)
     {
-        if(r[i]==cl[i]&&r[i]==d[i]&&cl[i]==d[i])
+        if(r[i]==cl[i])
             ans=1;
         else
         {
             ans=0;break;
         }
     }
-    if(ans==1)
+    if(ans==1&&dd==1)
         printf("no");
     else
         printf("yes");
