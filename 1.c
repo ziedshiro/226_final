1e8ecf4e7695a18af438519e235592e1fae40060
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-29 18:42:49 +0700

    1

--- 1.c
+++ 1.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
-int num(int n)
+int num(int n,int m)
 {
-
+    int i,k,ans=0;
+    k=n;
+    do
+    {
+        if(k%10==m)
+        {
+            ans=1;break;
+        }
+       k/=10;
+    }while(k>0);
+    return ans;
 }
 int main()
 {
@@ -9,9 +19,11 @@ int main()
     scanf("%d %d",&n,&m);
     do
     {
-        if(num(n)==0)
+        if(num(n,m)==0)
             n++;
         else
             ans=1;
+
     }while(ans!=1);
+    printf("%d",n);
 }
