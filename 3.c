f5f056273dd34c7feacba53e3ed683fc56314122
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-29 17:56:26 +0700

    3

--- 3.c
+++ 3.c
@@ -1,10 +1,33 @@
 #include<stdio.h>
+int row(int *n[],int x)
+{
+    int i,j;
+    for(i=0;i<x;i++)
+        for(j=0;j<x;j++)
+            printf("%d",n[i][j]);
+}
+int column(n)
+{
+
+}
+int bm(n)
+{
+
+}
 int main()
 {
     int x,y,z,i,j;
     scanf("%d %d %d",&x,&y,&z);
-    int a[x],b[y],c[z];
+    int a[x][x],b[y][y],c[z][z];
     for(i=0;i<x;i++)
         for(j=0;j<x;j++)
     scanf("%d",&a[i]);
+    for(i=0;i<y;i++)
+        for(j=0;j<y;j++)
+    scanf("%d",&b[i]);
+    for(i=0;i<z;i++)
+        for(j=0;j<z;j++)
+    scanf("%d",&c[i]);
+    row(a,x);
+
 }
