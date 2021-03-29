39f67ba88bd024bbf56c2390959163539fb6afc7
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-03-29 17:50:57 +0700

    3

--- 3.c
+++ 3.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
-    int x,y,z;
+    int x,y,z,i,j;
     scanf("%d %d %d",&x,&y,&z);
+    int a[x],b[y],c[z];
+    for(i=0;i<x;i++)
+        for(j=0;j<x;j++)
+    scanf("%d",&a[i]);
 }
